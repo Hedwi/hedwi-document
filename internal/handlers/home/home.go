16218b0f769d9426db2af78ed5f666a46535a2d7
solos <lxl1217@gmail.com>
2024-11-27 15:37:02 +0800

    add locale
    add locale

--- internal/handlers/home/home.go
+++ internal/handlers/home/home.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"hedwi-document/config"
+	"hedwi-document/utils"
 )
 
 func Home(c *gin.Context) {
@@ -14,6 +15,8 @@ func Home(c *gin.Context) {
 	_timestamp := config.DefaultConfig.VersionTimestamp
 	now := time.Now()
 	year := now.Format("2006")
-	c.HTML(http.StatusOK, "Home", gin.H{"_timestamp": _timestamp, "year": year})
+	locale := utils.GetLocale(utils.GetTag(utils.GetLocaleFromCookie(c), ""))
+
+	c.HTML(http.StatusOK, "Home", gin.H{"_timestamp": _timestamp, "year": year, "locale": locale})
 	return
 }
